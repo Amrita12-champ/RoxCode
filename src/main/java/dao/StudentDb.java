ad03dba852cbec69eb3f35b6d4dc156f96e4252f
Satyapriya Paikaray <satyapriyap67@gmail.com>
2026-08-24 12:03:55 +0530

    Admin Controller

--- src/main/java/dao/StudentDb.java
+++ src/main/java/dao/StudentDb.java
@@ -1,19 +1,20 @@
 package dao;
 
+import entity.Admin;
 import entity.Student;
 
 import java.sql.*;
 
 public class StudentDb {
-        private static final String url="jdbc:postgresql://localhost:5432/RoxCode";
-        private static final String user="postgres";
-        private static final String pass="Cutm@059";
+        private static final String url="jdbc:mysql://localhost:3306/RoxCode";
+        private static final String user="root";
+        private static final String pass="Satya@2005";
         private Connection con=null;
 
         public void connection(){
             try {
                 try {
-                    Class.forName("org.postgresql.Driver");
+                    Class.forName("com.mysql.cj.jdbc.Driver");
                 } catch (ClassNotFoundException ex) {
                     throw new RuntimeException(ex);
                 }
@@ -29,6 +30,8 @@ public class StudentDb {
                 prt.setString(1,a.getName());
                 prt.setString(2,a.getEmail());
                 prt.setString(3, a.getPassword());
+
+
 
                 return prt.executeUpdate();
             } catch (SQLException e) {
@@ -76,6 +79,66 @@ public class StudentDb {
                 throw new RuntimeException(e);
             }
         }
+
+
+        //Admin Databse
+
+
+        public int insert(Admin a){
+        String query="insert into Admin_register values (?,?,?)";
+        try {
+            PreparedStatement prt = con.prepareStatement(query);
+            prt.setString(1,a.getName());
+            prt.setString(2,a.getEmail());
+            prt.setString(3, a.getPassword());
+
+            return prt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+           }
+        }
+        public boolean delete(Admin a){
+        String query="delete from Admin_register where email=?";
+        try{
+            PreparedStatement prt = con.prepareStatement(query);
+                prt.setString(1, a.getEmail());
+                return prt.executeUpdate()>0;
+            }
+         catch (SQLException e) {
+            throw new RuntimeException(e);
+         }
+        }
+        public int update(Admin a){
+            String query="update Admin_register set name=?, Pass=? where email=?";
+            try {
+                PreparedStatement prt = con.prepareStatement(query);
+                prt.setString(1,a.getName());
+                prt.setString(2,a.getPassword());
+                prt.setString(3,a.getEmail());
+
+
+                return prt.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        public String SearchAdm(String email) {
+            String query="select * from Admin_register where email=?";
+            try{
+                PreparedStatement prt = con.prepareStatement(query);
+                prt.setString(1,email);
+                ResultSet rs = prt.executeQuery();
+                if(rs.next()){
+                    return rs.getString(2);
+                } else {
+                    return "Invalid";
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
     }
 
 
